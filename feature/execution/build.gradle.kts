6bb3a412ba761fe47d1d0928d2be26a0a82496ee
Mumsif <mumsifmumsif@gmail.com>
2026-07-30 18:06:52 +0530

    Commit before build the Setting's UI

--- feature/execution/build.gradle.kts
+++ feature/execution/build.gradle.kts
@@ -40,4 +40,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    debugImplementation(libs.androidx.compose.ui.tooling)
 }
--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -40,4 +40,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.foundation.layout)
+
+    debugImplementation(libs.androidx.compose.ui.tooling)
 }
